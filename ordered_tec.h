d4452ef7864eb45876c1c534c0e68e3424d1f8a5
luanmy <luan_ming_yi@126.com>
2018-01-24 23:41:37 +0800

    use liton_cpp_snippets

--- ordered_tec.h
+++ ordered_tec.h
@@ -39,7 +39,7 @@ namespace liton_ot
 
 	class TEC_FILE_BASE
 	{
-	public:
+	  public:
 		std::string FilePath;
 		std::string FileName;
 		std::string Title;
@@ -50,7 +50,7 @@ namespace liton_ot
 
 	class TEC_ZONE_BASE
 	{
-	public:
+	  public:
 		std::string ZoneName;
 		INT32 StrandId;
 		FLOAT64 SolutionTime;
@@ -67,8 +67,9 @@ namespace liton_ot
 
 	class TEC_DATA_BASE
 	{
-	public:
-		enum TEC_TYPE {
+	  public:
+		enum TEC_TYPE
+		{
 			TEC_NULL,
 			TEC_FLOAT = 1,
 			TEC_DOUBLE,
@@ -76,7 +77,7 @@ namespace liton_ot
 			TEC_SHORTINT,
 			TEC_BYTE
 		};
-	public:
+	  public:
 		TEC_TYPE type;
 		size_t size;
 	};
@@ -84,7 +85,7 @@ namespace liton_ot
 	class TEC_FILE_LOG : public TEC_FILE_BASE
 	{
 		friend class TEC_FILE;
-	public:
+	  public:
 		std::vector<TEC_ZONE_LOG> Zones;
 
 		std::string Time_Begin;
@@ -95,7 +96,7 @@ namespace liton_ot
 		std::vector<std::string> Echo_Text;
 		std::vector<std::string> Json_Text;
 		std::vector<std::string> Xml_Text;
-	public:
+	  public:
 		TEC_FILE_LOG();
 		explicit TEC_FILE_LOG(const TEC_FILE &file);
 
@@ -106,8 +107,8 @@ namespace liton_ot
 		void write_xml(int depth = 0);
 		void write_xml(std::ofstream &of, int depth = 0);
 
-		void read_xml(const tinyxml2::XMLElement * file_root);
-	protected:
+		void read_xml(const tinyxml2::XMLElement* file_root);
+	  protected:
 		void gen_json();
 		void gen_xml();
 	};
@@ -115,14 +116,14 @@ namespace liton_ot
 	class TEC_ZONE_LOG : public TEC_ZONE_BASE
 	{
 		friend class TEC_FILE_LOG;
-	public:
+	  public:
 		std::vector<TEC_DATA_LOG> Data;
 
 		double Size;
 		std::vector<std::string> Echo_Text;
 		std::vector<std::string> Json_Text;
 		std::vector<std::string> Xml_Text;
-	public:
+	  public:
 		TEC_ZONE_LOG();
 		explicit TEC_ZONE_LOG(const TEC_ZONE &zone);
 
@@ -133,33 +134,33 @@ namespace liton_ot
 		void write_xml(int depth = 0);
 		void write_xml(std::ofstream &of, int depth = 0);
 
-		void read_xml(const tinyxml2::XMLElement * zone_root);
-	protected:
+		void read_xml(const tinyxml2::XMLElement* zone_root);
+	  protected:
 		void gen_json();
 		void gen_xml();
 	};
 
 	class TEC_DATA_LOG : public TEC_DATA_BASE
 	{
-	public:
+	  public:
 		long int file_pt;
 		double min;
 		double max;
-	public:
+	  public:
 		TEC_DATA_LOG();
 		explicit TEC_DATA_LOG(const TEC_DATA &data);
 	};
 
 	class TEC_FILE : public TEC_FILE_BASE
 	{
-	public:
+	  public:
 		std::vector<TEC_ZONE> Zones;
 
 		//usingtime, time, size, section, variable, file_end, file_head
 		std::bitset<7> Echo_Mode;
 
 		TEC_FILE_LOG last_log;
-	public:
+	  public:
 		TEC_FILE(const std::string &name = "untitled_file", const std::string &path = ".", const std::string &title = "untitled");
 
 		bool add_auxiliary_data(const std::string &name, const std::string &value);
@@ -168,33 +169,33 @@ namespace liton_ot
 		void set_echo_mode(const std::string &file, const std::string &zone);
 
 		void write_plt(bool echo = true);
-	protected:
+	  protected:
 		void echo_mode(const std::string &echo = "brief");
 
 		void wrtie_plt_pre();
-		void write_plt_head(FILE *of, bool echo = true);
-		void write_plt_data(FILE *of, bool echo = true);
+		void write_plt_head(FILE* of, bool echo = true);
+		void write_plt_data(FILE* of, bool echo = true);
 	};
 
 	class TEC_ZONE : public TEC_ZONE_BASE
 	{
 		friend class TEC_FILE;
-	public:
+	  public:
 		std::vector<TEC_DATA> Data;
 
 		//size, stdid & soltime, begin & end, skip, max_org, max_real, variable, zone_end, zone_head
 		std::bitset<9> Echo_Mode;
-	protected:
+	  protected:
 		INT32 Real_Max[3];
 		INT32 Real_Dim;
 		bool noskip, noexc;
 		bool needreal;
-	public:
+	  public:
 		explicit TEC_ZONE(const std::string &name = "untitled_zone");
-		const INT32 * get_real_size(const std::string &name = "realmax");
+		const INT32* get_real_size(const std::string &name = "realmax");
 		bool add_auxiliary_data(const std::string &name, const std::string &value);
 		bool add_auxiliary_data(const std::string &name, const double &value);
-	protected:
+	  protected:
 		void echo_mode(const std::string &echo = "brief");
 
 		void gather_real_size();
@@ -202,52 +203,56 @@ namespace liton_ot
 		void realise_buf();
 
 		void wrtie_plt_pre(const TEC_FILE &thisfile, TEC_ZONE_LOG &zone_log);
-		void write_plt_head(FILE *of) const;
-		void write_plt_data(FILE *of, const TEC_FILE &thisfile, TEC_ZONE_LOG &zone_log, bool echo = true);
+		void write_plt_head(FILE* of) const;
+		void write_plt_data(FILE* of, const TEC_FILE &thisfile, TEC_ZONE_LOG &zone_log, bool echo = true);
 	};
 
 	class TEC_DATA : public TEC_DATA_BASE
 	{
 		friend class TEC_ZONE;
-	public:
-		const void * DataP;
-	protected:
-		byte * buf;
-	public:
+	  public:
+		const void* DataP;
+	  protected:
+		byte* buf;
+	  public:
 		TEC_DATA();
-		template<typename T> explicit TEC_DATA(T * iDataP);
+		template<typename T> explicit TEC_DATA(T* iDataP);
 
-	protected:
+	  protected:
 		std::pair<FLOAT64, FLOAT64> minmax(size_t N) const;
 	};
 }
 
-template<typename T> liton_ot::TEC_DATA::TEC_DATA(T * iDataP)
+template<typename T> liton_ot::TEC_DATA::TEC_DATA(T* iDataP)
 {
-	if (typeid(iDataP) == typeid(float *))
+	if (typeid(iDataP) == typeid(float*))
 	{
 		type = TEC_DATA::TEC_FLOAT;
 	}
-	else if (typeid(iDataP) == typeid(double *))
+	else if (typeid(iDataP) == typeid(double*))
 	{
 		type = TEC_DATA::TEC_DOUBLE;
 	}
-	else if ((typeid(iDataP) == typeid(longint *) || typeid(iDataP) == typeid(int *)) && sizeof(T) == TEC_LONGINT_S)
+	else if ((typeid(iDataP) == typeid(longint*) || typeid(iDataP) == typeid(int*)) && sizeof(T) == TEC_LONGINT_S)
 	{
 		type = TEC_DATA::TEC_LONGINT;
 	}
-	else if ((typeid(iDataP) == typeid(shortint *) || typeid(iDataP) == typeid(int *)) && sizeof(T) == TEC_SHORTINT_S)
+	else if ((typeid(iDataP) == typeid(shortint*) || typeid(iDataP) == typeid(int*)) && sizeof(T) == TEC_SHORTINT_S)
 	{
 		type = TEC_DATA::TEC_SHORTINT;
 	}
-	else if (typeid(iDataP) == typeid(byte *))
+	else if (typeid(iDataP) == typeid(byte*))
 	{
 		type = TEC_DATA::TEC_BYTE;
 	}
 	else
 	{
 		char buf[300];
+#ifdef _MSC_VER
+		sprintf_s(buf, "type [%s]*%i is unsupported in Tecplot", typeid(T).name(), static_cast<int>(sizeof(T)));
+#else
 		std::sprintf(buf, "type [%s]*%i is unsupported in Tecplot", typeid(T).name(), static_cast<int>(sizeof(T)));
+#endif
 		throw(std::runtime_error(buf));
 	}
 	DataP = iDataP;
