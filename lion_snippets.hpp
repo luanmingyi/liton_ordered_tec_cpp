55181fc07e2b8e49cb86c6a7ad72fa4f58d9bd7c
luanmy <luan_ming_yi@126.com>
2018-01-26 23:47:19 +0800

    add date and rand number to exec_except

--- lion_snippets.hpp
+++ lion_snippets.hpp
@@ -1,7 +1,11 @@
 #ifndef LITON_SNIPPET_HPP
 #define LITON_SNIPPET_HPP
 
+#include <cstdlib>
 #include <cstdio>
+
+#include <sstream>
+#include <iomanip>
 #include <string>
 #include <stdexcept>
 #include <ctime>
@@ -99,15 +103,18 @@ namespace liton_sp
 		template<typename Expression>
 		void exec_except(const Expression &exp, ostream &out, ostream &except_out)
 		{
+			ostringstream tran;
+			tran << std::setw(10) << std::setfill('0') << std::rand();
+			std::string timestr = "[" + time::get_time() + "|" + tran.str() + "]: ";
 			try
 			{
 				exp();
-				out << "no exception" << std::endl;
+				out << timestr << "no exception" << std::endl;
 			}
 			catch (const std::exception &err)
 			{
-				out << "with exception" << std::endl;
-				except_out << err.what() << std::endl;
+				out << timestr << "with exception" << std::endl;
+				except_out << timestr << err.what() << std::endl;
 			}
 		}
 
