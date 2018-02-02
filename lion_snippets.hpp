67e8bc523cdef8345836b09905a5d5856a57e725
luanmy <luan_ming_yi@126.com>
2018-02-03 01:24:45 +0800

    change static const char * to static function member

--- lion_snippets.hpp
+++ lion_snippets.hpp
@@ -58,53 +58,57 @@ namespace liton_sp
 	class env
 	{
 	  public:
-		static const char* os;
-		static const char* compiler;
-		static const char* cpp_std;
-		static const char* debug_mode;
+		static const char* os()
+		{
+#ifdef _WIN32
+			return "Windows 32 or Windows 64";
+#elif _WIN64
+			return "Windows 64";
+#elif __MINGW32__
+			return "Windows32 by mingw compiler";
+#elif __CYGWIN__
+			return "Cygwin";
+#elif __linux__
+			return "linux";
+#endif
+		}
+		static const char* compiler()
+		{
+#ifdef __INTEL_COMPILER
+			return "Interl C++";
+#elif _MSC_VER
+			return "Visual C++";
+#elif __GNUC__
+			return "GCC";
+#endif
+		}
+		static const char* cpp_std()
+		{
+#if (__cplusplus < 201103L && !defined(_MSC_VER)) || (defined(_MSC_VER) && (_MSC_VER < 1700))
+			return "C++03";
+#else
+			return "C++11";
+#endif
+		}
+		static const char* debug_mode()
+		{
+#if defined(_DEBUG) || defined(DEBUG)
+#ifndef _DEBUG
+#define _DEBUG
+#endif
+			return "DEBUG";
+#elif defined(_NDEBUG) || !defined(_DEBUG)
+			return "RELEASE";
+#endif
+		}
 		static void disp_env(std::ostream &out)
 		{
-			out << os << std::endl;
-			out << compiler << std::endl;
-			out << cpp_std << std::endl;
-			out << debug_mode << std::endl;
+			out << os() << std::endl;
+			out << compiler() << std::endl;
+			out << cpp_std() << std::endl;
+			out << debug_mode() << std::endl;
 		}
 	};
-
-#ifdef _WIN32
-	const char* env::os = "Windows 32 or Windows 64";
-#elif _WIN64
-	const char* env::os = "Windows 64";
-#elif __MINGW32__
-	const char* env::os = "Windows32 by mingw compiler";
-#elif __CYGWIN__
-	const char* env::os = "Cygwin";
-#elif __linux__
-	const char* env::os = "linux";
-#endif
-
-#ifdef __INTEL_COMPILER
-	const char* env::compiler = "Interl C++";
-#elif _MSC_VER
-	const char* env::compiler = "Visual C++";
-#elif __GNUC__
-	const char* env::compiler = "GCC";
-#endif
-
-#if (__cplusplus < 201103L && !defined(_MSC_VER)) || (defined(_MSC_VER) && (_MSC_VER < 1700))
-	const char* env::cpp_std = "C++03";
-#else
-	const char* env::cpp_std = "C++11";
-#endif
-
-#if defined(_DEBUG) || defined(DEBUG)
-	#ifndef _DEBUG
-		#define _DEBUG
-	#endif
-	const char* env::debug_mode = "DEBUG";
-#elif defined(_NDEBUG) || !defined(_DEBUG)
-	const char* env::debug_mode = "RELEASE";
-#endif
 
 	class debug
 	{
